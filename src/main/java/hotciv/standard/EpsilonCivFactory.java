a9729c9025d3c103ec06b06b9667420fcafec3f8
Benjamin Nguyen <ben30@pitt.edu>
2021-10-31 22:09:36 -0400

    add factoryType method to GameFactory interface

--- src/main/java/hotciv/standard/EpsilonCivFactory.java
+++ src/main/java/hotciv/standard/EpsilonCivFactory.java
@@ -14,4 +14,5 @@ public class EpsilonCivFactory implements GameFactory  {
     public StartCitiesStrategy createStartCitiesStrategy() { return new AlphaCivStartCitiesStrategy(); }
     public StartUnitsStrategy createStartUnitsStrategy() { return new AlphaCivStartUnitsStrategy(); }
     public AttackingStrategy createAttackingStrategy(){ return new EpsilonCivAttackingStrategy(useTestStub); }
+    public String factoryType() { return "EpsilonCivFactory"; }
 }
